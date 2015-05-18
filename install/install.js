65e3ff966f28b5f3f4779e11c9c194ef10f3b97f
Asheesh Laroia <asheesh@asheesh.org>
2015-05-17 17:31:16 -0700

    Use more verbose element ID for app name

--- install/install.js
+++ install/install.js
@@ -1,7 +1,7 @@
 function updatePageTitle() {
   var hash = window.location.hash.replace("#", "");
   if (hash) {
-    var span = document.getElementById("js-replace");
+    var span = document.getElementById("js-replace-appname");
     span.textContent = "the easiest way to run " + hash;
   }
 }
