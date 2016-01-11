638608ad0684a1c851e9b6a4201bc277a33043d1
Kenton Varda <kenton@sandstorm.io>
2016-01-11 15:20:26 -0800

    Update push-site.sh to push generated content to github.

--- regenerate-site.sh
+++ regenerate-site.sh
@@ -3,7 +3,7 @@
 set -eu
 shopt -s extglob
 
-if (git grep 'localhost:4000' | egrep -v "(README|push-site)"); then
+if (git grep 'localhost:4000' | egrep -v "(README|regenerate-site)"); then
   echo "ERROR: Your content has links to localhost:4000!" >&2
   exit 1
 fi
@@ -21,27 +21,40 @@ case $(git rev-parse --abbrev-ref HEAD) in
   master )
     echo "On master branch.  Will push to sandstorm.io."
     HOST=sandstorm.io
+    BRANCH=published
     ;;
 
   * )
     echo "On devel branch.  Will push to next.sandstorm.io."
     HOST=next.sandstorm.io
+    BRANCH=published-next
     ;;
 esac
 
 echo "Regenerating site..."
 
-rm -rf _pushsite _pushsite.tar.gz
-
+rm -rf _pushsite
 jekyll build --safe -d _pushsite
 
-echo -n "Push now? (y/N)"
+rm -rf _published
+mkdir _published
+
+(cd _published && git clone -b "$BRANCH" .. .)
+
+# Note that these intentionally skip dotfiles.
+rm -rf _published/*
+cp -r _pushsite/* _published
+
+COMMIT=$(git rev-parse HEAD)
+(cd _published && git add . && git commit -am "Regenerated site from commit: $COMMIT")
+
+echo -n "Push to github? (y/N)"
 read -n 1 YESNO
 echo
 
 if [ "x$YESNO" == "xy" ]; then
-  echo "Pushing..."
-  tar cJ --xform='s,_pushsite/,,' _pushsite/* | gce-ss ssh fe --command "cd /var/www/$HOST && tar xJ"
+  (cd _published && git push)
+  echo git push -u origin "$BRANCH:$BRANCH"
 else
   echo "Push CANCELED"
 fi
