2425a5ae1ef31f9022080b4d63b692a97011a77e
Drew Fisher <drew@sandstorm.io>
2016-11-09 15:13:12 -0800

    Add experiments framework
    We inline the script needed to support this in the page <head> so we
    can generate stylesheets according to experiment membership *before*
    any page content is displayed.  Otherwise, we'd potentially wind up
    with flash of unstyled content or page reflow, which is undesirable.
    
    As a result of the above inlining of all the early script, stats.js is no
    longer used directly by this site, but remains as a publicly-consumable
    endpoint for use by other sites that src it.  This change also exposes
    the experiment set (including membership), which should allow us to
    maintain a single copy of experiment rules/membership sets that will
    be used by all of our properties.
    
    This change does not actually enable any experiments; they will come
    in future changes.

--- stats.js
+++ stats.js
@@ -1,64 +1,23 @@
+---
+# empty front matter to convince jekyll this file should be rendered like a template
+# and receive inclusions and liquid blocks.
+---
+{% if false %}
+// Intended for inclusion by sites that are not this one, like the SfW dashboard
+// Hangs an object `sandstormTracker` off the window object, with hooks to:
+//  - submit a pageview event
+//  - expose the tracker ID
+//  - see what experiments the viewer is opted into
+{% endif %}
 (function () {
-  function getIdCookie() {
-    var match = document.cookie.match(new RegExp("(^| )sandstormStatsId=([^;]+)"));
-    if (match) return match[2];
-  }
+  {% include stats-support.js %}
 
-  function setIdCookie(val) {
-    var d = new Date();
-    d.setMonth(d.getMonth() + 120);
-    document.cookie = "sandstormStatsId=" + val + ";expires=" + d + ";domain=.sandstorm.io;";
-  }
+  window.sandstormTrackView = trackView; // TODO: remove once feature key vendor uses window.sandstormTracker.trackView
+  window.sandstormTracker = {
+    trackView: trackView,
+    experiments: experiments,
+    getId: getId
+  };
 
-  function uidGenerator() {
-    if (window.crypto && window.crypto.getRandomValues) {
-      var array = new Uint32Array(4);
-      window.crypto.getRandomValues(array);
-      return array[0] + "-" + array[1] + "-" + array[2] + "-" + array[3];
-    } else {
-      var genInt = function() {
-        return Math.floor(Math.random() * 0x100000000);
-      }
-      return genInt() + "-" + genInt() + "-" + genInt() + "-" + genInt();
-    }
-  }
-
-  function sendData(data) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if (xhr.status >= 200 && xhr.status < 300) {
-          // do nothing
-        } else {
-          console.error("analytics send failed:", xhr.statusText, xhr.responseText);
-        }
-      }
-    };
-
-    xhr.open("POST", "https://api-5d23e9c497f90642a85ba1fb909fa9d8.oasis.sandstorm.io/pageEvent", true);
-
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.setRequestHeader("Authorization", "Bearer hVU87Vp2UzNnRmMepiXpjKUk6n6MZc7YMgAJ6YrVe-6");
-
-    xhr.send(JSON.stringify(data));
-  }
-
-  function sandstormTrackView() {
-    var id = getIdCookie();
-    if (!id) {
-      id = uidGenerator();
-      setIdCookie(id);
-    }
-
-    sendData({
-      uid: id,
-      referrer: document.referrer,
-      location: window.location + '',
-    });
-  }
-
-  window.sandstormTrackView = sandstormTrackView;
-
-  sandstormTrackView();
+  trackView();
 })();
